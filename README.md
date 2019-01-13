7f4eccf90535a216b014047ef0796c0abcd16ba1
Richard Dallaway <richard@dallaway.com>
2019-01-13 17:50:23 +0000

    using a struct as an exercise

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # Compute [Hardy-Weinberg Equilibrium](https://en.wikipedia.org/wiki/Hardy%E2%80%93Weinberg_principle) Values
 
-Call with allele counts for AA Aa aa  on the command line:
+Call with allele counts for AA Aa aa on the command line:
 
 ```
 $ cargo run 664 435 116
-    Finished dev [unoptimized + debuginfo] target(s) in 0.05s
+   Compiling hwcalc v0.1.0 (/Users/richard/Developer/rust/hwcalc)
+    Finished dev [unoptimized + debuginfo] target(s) in 0.98s
      Running `target/debug/hwcalc 664 435 116`
+N = 1215
+p(A) = 0.73 (frequency of A)
+q(a) = 0.27 (frequency of a)
 Observed genotype frequencies: [0.55, 0.36, 0.10]
-p^2 = 0.53
-2pq = 0.40
-q^2 = 0.08
+p^2 = 0.53 (next generation AA under random mating)
+2pq = 0.40 (next generation Aa)
+q^2 = 0.08 (next generation aa)
 F_ST = 0.10
 ```
 
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,47 @@
 use std::env;
 
-#[allow(non_snake_case)]
-fn main() {
+struct HW {
+    n: f64,
+    freqs: [f64; 3],
+    p: f64,
+    q: f64,
+    heterozygous: f64,
+}
 
+impl HW {
+    #[allow(non_snake_case)]
+    pub fn new(AA: f64, Aa: f64, aa: f64) -> Self {
+        let n = AA + Aa + aa;
+        let (AA, Aa, aa) = (AA / n, Aa / n, aa / n);
+        Self {
+            n,
+            freqs: [AA, Aa, aa],
+            p: AA + (0.5 * Aa),
+            q: aa + (0.5 * Aa),
+            heterozygous: Aa,
+        }
+    }
+}
+
+fn main() {
     let args: Vec<f64> = env::args().flat_map(|s| s.parse()).collect();
     if args.len() != 3 {
         println!("Usage: hwcalc AA_count Aa_count aa_count");
     } else {
-        let total: f64 = args.iter().sum();
-        let freq: Vec<f64> = args.iter().map(|c| c / total).collect();
-        println!("Observed genotype frequencies: {:.2?}", freq);
+        let hw = HW::new(args[0], args[1], args[2]);
 
-        let (AA, Aa, aa) = (freq[0], freq[1], freq[2]);
-        let p = AA + (0.5 * Aa);
-        let q = aa + (0.5 * Aa);
+        println!("N = {:.0}", hw.n);
+        println!("p(A) = {:.2} (frequency of A)", hw.p);
+        println!("q(a) = {:.2} (frequency of a)", hw.q);
+        println!("Observed genotype frequencies: {:.2?}", hw.freqs);
 
-        println!("p(A) = {:.2}", p);
-        println!("q(a) = {:.2}", q);
+        let hw_2pq = 2.0 * hw.p * hw.q;
 
-        let hw_2pq = 2.0 * p * q;
+        println!("p^2 = {:.2} (next generation AA under random mating)", hw.p.powi(2));
+        println!("2pq = {:.2} (next generation Aa)", hw_2pq);
+        println!("q^2 = {:.2} (next generation aa)", hw.q.powi(2));
 
-        println!("p^2 = {:.2}", p.powi(2) );
-        println!("2pq = {:.2}", hw_2pq);
-        println!("q^2 = {:.2}", q.powi(2));
-
-        let F_ST = (hw_2pq - Aa) / hw_2pq;
-        println!("F_ST = {:.2}", F_ST);
+        let f_st = (hw_2pq - hw.heterozygous) / hw_2pq;
+        println!("F_ST = {:.2}", f_st);
     }
 }
